86d03e7546873f36232bab4dceb43f2e784e7f94
Arryan Bhatnagar <bhatnag8@purdue.edu>
2024-01-31 10:29:08 -0500

    Initial Commit
    - Committing base files for the project

---
+++ Sweat Social/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Sweat Social
+//
+//  Created by Arryan Bhatnagar on 1/31/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Sweat Social/Sweat_SocialApp.swift
@@ -0,0 +1,17 @@
+//
+//  Sweat_SocialApp.swift
+//  Sweat Social
+//
+//  Created by Arryan Bhatnagar on 1/31/24.
+//
+
+import SwiftUI
+
+@main
+struct Sweat_SocialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
