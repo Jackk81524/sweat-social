bfd3139cff5af0137b4ce7b4d0fe1ecd35376036
Jack Knox <knox36@purdue.edu>
2024-02-09 14:32:39 -0500

    Add a login button, add navigation view
    Add a login button, no action yet

--- Sweat Social/Views/LoginView.swift
+++ Sweat Social/Views/LoginView.swift
@@ -12,45 +12,63 @@ struct LoginView: View {
     @State var password = ""
     
     var body: some View {
-        VStack {
-            ZStack {
-                Ellipse()
-                    .foregroundColor(.black)
-                
-                Text("Sweat Social")
-                    .font(.custom("Futura-MediumItalic", size: 64))
-                    .fontWidth(.condensed)
-                    .foregroundStyle(.white)
-                    .baselineOffset(-135)
-                    .bold()
-                
-            }
-            .frame(width: UIScreen.main.bounds.width * 2.3, height: 259)
-            .offset(y: -315)
-            
+        NavigationView {
             VStack {
-                Text("Already have an account?")
-                    .font(.system(size:26))
-                    .padding(.bottom, 20)
+                ZStack {
+                    Ellipse()
+                        .foregroundColor(.black)
+                    
+                    Text("Sweat Social")
+                        .font(.custom("Futura-MediumItalic", size: 64))
+                        .fontWidth(.condensed)
+                        .foregroundStyle(.white)
+                        .baselineOffset(-135)
+                        .bold()
+                    
+                }
+                .frame(width: UIScreen.main.bounds.width * 2.3, height: 259)
+                .offset(y: -190)
+                .padding()
                 
-                
-                TextField("Email Address", text: $email)
-                    .padding()
-                    .frame(width:306,height: 45)
-                    .overlay(
-                        RoundedRectangle(cornerRadius: 16)
-                            .strokeBorder(Color.black,lineWidth:2)
-                    )
-                SecureField("Password", text: $password)
-                    .padding()
-                    .frame(width:306,height: 45)
-                    .overlay(
-                        RoundedRectangle(cornerRadius: 16)
-                            .strokeBorder(Color.black,lineWidth:2)
-                    )
-                
+                VStack {
+                    Text("Already have an account?")
+                        .font(.system(size:26))
+                        .padding(.bottom, 20)
+                    
+                    
+                    TextField("Email Address", text: $email)
+                        .padding()
+                        .frame(width:306,height: 45)
+                        .overlay(
+                            RoundedRectangle(cornerRadius: 16)
+                                .strokeBorder(Color.black,lineWidth:2)
+                        )
+                    SecureField("Password", text: $password)
+                        .padding()
+                        .frame(width:306,height: 45)
+                        .overlay(
+                            RoundedRectangle(cornerRadius: 16)
+                                .strokeBorder(Color.black,lineWidth:2)
+                        )
+                    
+                    Button {
+                        //Attemp login
+                    } label: {
+                        ZStack {
+                            RoundedRectangle(cornerRadius: 32)
+                                .foregroundColor(.black)
+                            Text("Login")
+                                .foregroundColor(.white)
+                                .font(.system(size:16))
+                                .fontWeight(/*@START_MENU_TOKEN@*/.bold/*@END_MENU_TOKEN@*/)
+                        }
+                        .frame(width: 141, height: 45)
+                        .padding()
+                    }
+                    
+                }
+                .offset(y:-200)
             }
-            .offset(y:-275)
         }
     }
 }
