b714c93e7e6ec4491c59cabc23e407fb4310692e
Jack Knox <knox36@purdue.edu>
2024-02-14 12:38:33 -0500

    Updated register view #19 , have model, viewModel, and view
    Did work to begin creating the register view. Have baseline done, still some work. Need to fix view, as the header is in different spots on register and login. Also need to confirm passwords match. No connection to firebase yet.

---
+++ Sweat Social/Other/Extensions.swift
@@ -0,0 +1,24 @@
+//
+//  Extensions.swift
+//  Sweat Social
+//
+//  Created by Jack.Knox on 2024-02-14.
+//
+
+import Foundation
+
+extension Encodable {
+    func asDictionary() -> [String:Any] {
+        guard let data = try? JSONEncoder().encode(self) else {
+            return [:]
+        }
+        
+        do {
+            let json = try JSONSerialization.jsonObject(with: data) as? [String: Any]
+            return json ?? [:]
+        } catch {
+            return [:]
+        }
+        
+    }
+}
--- Sweat Social/Views/RegisterView.swift
+++ Sweat Social/Views/RegisterView.swift
@@ -8,11 +8,41 @@
 import SwiftUI
 
 struct RegisterView: View {
+    @StateObject var viewModel = RegisterViewModel()
+    
     var body: some View {
-        VStack {
-            EntryHeaderView()
-            
-            
+        NavigationView {
+            VStack {
+                EntryHeaderView()
+                
+                VStack {
+                    Text("Register with us now")
+                        .font(.system(size:26))
+                        .padding(.bottom, 20)
+                    
+                    EntryTextFieldView(display: "Full Name",
+                                       input: $viewModel.name)
+                    EntryTextFieldView(display: "Email",
+                                       input: $viewModel.email)
+                    EntryPasswordView(display: "Password",
+                                      input: $viewModel.password)
+                    EntryPasswordView(display: "Confirm Password",
+                                      input: $viewModel.confirm_password)
+                    
+                    EntryButtonView(title: "Sign up")
+                    {
+                        viewModel.register()
+                    }
+                }
+                .offset(y:-200)
+                
+                VStack {
+                    Text("Already have an account?")
+                    NavigationLink("Log in", destination: LoginView())
+                }
+                
+                
+            }
         }
     }
 }
