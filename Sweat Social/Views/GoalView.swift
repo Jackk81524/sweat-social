1abdba86030b074b69fd07cbc804022864937e94
CtrlAltDelight <git+git@ctrlaltdelight.org>
2024-02-15 15:12:37 -0500

    Made new view SwiftUI file

---
+++ Sweat Social/Views/GoalView.swift
@@ -0,0 +1,18 @@
+//
+//  GoalView.swift
+//  Sweat Social
+//
+//  Created by Luke Chigges on 2/15/24.
+//
+
+import SwiftUI
+
+struct GoalView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    GoalView()
+}
