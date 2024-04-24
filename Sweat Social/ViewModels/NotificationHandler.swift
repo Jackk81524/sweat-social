e13673fc90121bb38bf14ff46dfa7be737e3d595
CtrlAltDelight <git+git@ctrlaltdelight.org>
2024-04-24 15:58:15 -0400

    Added function to more easily push a notification out.

--- Sweat Social/ViewModels/NotificationHandler.swift
+++ Sweat Social/ViewModels/NotificationHandler.swift
@@ -96,3 +96,23 @@ func scheduleLocalNotification() {
         }
     }
 }
+
+func sendPushNotification(title: String, body: String) {
+    let content = UNMutableNotificationContent()
+    content.title = title
+    content.body = body
+    content.sound = UNNotificationSound.default
+
+    // Trigger in 1 second
+    let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 1, repeats: false)
+
+    // Create the request
+    let request = UNNotificationRequest(identifier: UUID().uuidString, content: content, trigger: trigger)
+
+    // Add request to UNUserNotificationCenter
+    UNUserNotificationCenter.current().add(request) { error in
+        if let error = error {
+            print("Error scheduling local notification: \(error)")
+        }
+    }
+}
---
+++ Sweat Social/Views/AchievementsView.swift
@@ -0,0 +1,18 @@
+//
+//  AchievementsView.swift
+//  Sweat Social
+//
+//  Created by Luke Chigges on 4/24/24.
+//
+
+import SwiftUI
+
+struct AchievementsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AchievementsView()
+}
